e7b6b6be77002a54f35116cd94fc850ac2365dcc
alex <alexragland2003@gmail.com>
2024-11-17 14:43:14 -0800

    view and create snippets handlers

--- main.go
+++ main.go
@@ -9,10 +9,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form to create a new snippet"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on localhost:8080")
 
