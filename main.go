e01d04cdc0feaa9f0bbff95e553526f43200f216
alex <alexragland2003@gmail.com>
2024-11-17 00:26:43 -0800

    hello world

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello world")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", home)
+
+	log.Print("Starting server on localhost:8080")
+
+	err := http.ListenAndServe("localhost:8080", mux)
+	log.Fatal(err)
 }
